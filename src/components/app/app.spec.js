10ecb112cee1f35abf0d9022bd2d3f8eb37228e4
Stevo99 <Sfailla1983@gmail.com>
2020-06-24 20:46:55 -0400

    added enzyme and config to boilerplate. added initial tests for app

--- src/components/app/app.spec.js
+++ src/components/app/app.spec.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+// import { render } from '@testing-library/react';
+import { shallow, render } from 'enzyme';
+
 import App from './App';
 
-describe('App', () => {
-	it('renders App without error', () => {
+describe('<App />', () => {
+	it('should render app', () => {
+		const wrapper = shallow(<App />);
 		render(<App />);
+	});
+
+	it('should find a <div> tag and <code> tag', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('div').length).toBe(2);
+		expect(wrapper.find('code').length).toBe(1);
 	});
 });
