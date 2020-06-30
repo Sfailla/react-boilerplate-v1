f50cda735f55aebcba2a36686f9d5f022685365a
Stevo99 <Sfailla1983@gmail.com>
2020-06-29 20:11:18 -0400

    changed app from sass to jss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import 'normalize.css';
-import './styles/style.scss';
+import './index.css';
 import App from './components/app/app';
 
 const app = <App />;
